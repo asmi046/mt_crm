200b7f773450224b7d11e9e7f65c27f816d98cbf
asmi046 <asmi046@gmail.com>
2024-03-26 21:24:43 +0300

    Формы

--- resources/views/user-data.blade.php
+++ resources/views/user-data.blade.php
@@ -21,7 +21,89 @@
             </div>
 
             <div class="box pt_10">
+                <h3>Данные пользователя</h3>
+                <form action="#" method="post" class="box">
+                    @csrf
 
+                    <div class="field">
+                        <label class="label">Ф.И.О<span class="require">*</span></label>
+                        <div class="control">
+                          <input name="name" class="input" type="text" placeholder="Введите имя">
+                        </div>
+
+                        @error('name')
+                          <p class="error">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="field">
+                        <label class="label">E-mail</label>
+                        <div class="control">
+                          <input name="email" class="input" type="email" disabled placeholder="e.g. alex@example.com">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Телефон<span class="require">*</span></label>
+                        <div class="control">
+                          <input name="phone" class="input" type="tel" placeholder="Введите телефон">
+                        </div>
+
+                        @error('phone')
+                          <p class="error">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Агентство<span class="require">*</span></label>
+                        <div class="control">
+                            <input name="agency" class="input" type="text" placeholder="Введите название агентства">
+                        </div>
+
+                        @error('agency')
+                          <p class="error">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <footer>
+                        <button type="submit" class="button ">Сохранить</button>
+                    </footer>
+
+                </form>
+                <br>
+                <hr>
+                <br>
+                <h3>Смена пароля</h3>
+                <form action="#" method="post" class="box">
+                    @csrf
+
+                    <div class="field">
+                        <label class="label">Введите новый пароль</label>
+                        <div class="control">
+                          <input name="password" class="input" type="password" placeholder="********">
+                        </div>
+
+                        @error('password')
+                            <p class="error">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Подтвердите пароль</label>
+                        <div class="control">
+                          <input name="password_confirmation" class="input" type="password" placeholder="********">
+                        </div>
+
+                        @error('password_confirmation')
+                            <p class="error">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <footer>
+                        <button type="submit" class="button ">Сменить пароль</button>
+                    </footer>
+
+                </form>
             </div>
 
         </div>
